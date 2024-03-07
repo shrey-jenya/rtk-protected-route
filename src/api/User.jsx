af9020ad34d2056795e3db5124a0692f99c872f1
Shrey Patel <shrey.jenya@gmail.com>
2024-03-07 09:27:16 +0530

    delete added

--- src/api/User.jsx
+++ src/api/User.jsx
@@ -1,62 +1,46 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 const User = () => {
-    const navigate = useNavigate()
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-    const [users, setUsers] = useState([])
+    const navigate = useNavigate();
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const dispatch = useDispatch();
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
-        fetchUsers()
-        console.log('user is authenticated')
-    }, [])
+        fetchUsers();
+    }, []);
+
     const fetchUsers = async () => {
         try {
-            const response = await axios.get(`https://dummyjson.com/users`)
-            setUsers(response.data.users)
-            console.log(response.data.users)
+            const response = await axios.get(`https://dummyjson.com/users`);
+            setUsers(response.data.users);
         } catch (error) {
             console.error(error);
         }
-    }
-    const addUsers = async () => {
-        try {
-            const response = await axios.post(`https://dummyjson.com/users/add`)
-            console.log('user added successfully')
-            fetchUsers(response)
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    const deleteUsers = async (id) => {
-        try {
-            await axios.delete(`https://dummyjson.com/users/${id}`)
-            fetchUsers()
-            console.log('user deleted successfully')
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    const updateUsers = async (id) => {
-        try {
-            await axios.put(`https://dummyjson.com/users/update/${id}`)
-            fetchUsers()
-            console.log('user updated successfully')
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    };
+
     useEffect(() => {
         if (!isAuthenticated) {
-            navigate('/login')
+            navigate('/login');
         }
-    }, [isAuthenticated, navigate])
+    }, [isAuthenticated, navigate]);
+
+    const deleteUser = async (userId) => {
+        try {
+            await axios.delete(`https://dummyjson.com/users/${userId}`);
+            const updatedUsers = users.filter(user => user.id !== userId);
+            setUsers(updatedUsers);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
             <section className="container px-4 mx-auto mt-3">
-                <button onClick={addUsers} className="px-6 float-right py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                    Add User
-                </button>
                 <div className="flex items-center gap-x-3">
                     <h2 className="text-lg font-medium text-gray-800 dark:text-white">Team members</h2>
                     <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{users.length}</span>
@@ -86,45 +70,43 @@ const User = () => {
                                         </tr>
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                                        {
-                                            users.map((user) => (
-                                                <tr key={user.id}>
-                                                    <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                        <div className="inline-flex items-center gap-x-3">
-                                                            <input type="checkbox" className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
-                                                            <div className="flex items-center gap-x-2">
-                                                                <img className="object-cover w-10 h-10 rounded-full" src={user.image} alt={user.firstName} />
-                                                                <div>
-                                                                    <h2 className="font-medium text-gray-800 dark:text-white ">{user.firstName}</h2>
-                                                                    <p className="text-sm font-normal text-gray-600 dark:text-gray-400">{user.lastName}</p>
-                                                                </div>
+                                        {users.map((user) => (
+                                            <tr key={user.id}>
+                                                <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                    <div className="inline-flex items-center gap-x-3">
+                                                        <input type="checkbox" className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700" />
+                                                        <div className="flex items-center gap-x-2">
+                                                            <img className="object-cover w-10 h-10 rounded-full" src={user.image} alt={user.firstName} />
+                                                            <div>
+                                                                <h2 className="font-medium text-gray-800 dark:text-white ">{user.firstName}</h2>
+                                                                <p className="text-sm font-normal text-gray-600 dark:text-gray-400">{user.lastName}</p>
                                                             </div>
                                                         </div>
-                                                    </td>
-                                                    <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                                        <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
-                                                            <span className="h-1.5 w-1.5 rounded-full bg-emerald-500"></span>
-                                                            <h2 className="text-sm font-normal text-emerald-500">{user.address.city}</h2>
-                                                        </div>
-                                                    </td>
-                                                    <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{user.email}</td>
-                                                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                                                        <div className="flex items-center gap-x-6">
-                                                            <button onClick={()=>deleteUsers(user.id)} className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                                                                </svg>
-                                                            </button>
-                                                            <button onClick={updateUsers} className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                                                </svg>
-                                                            </button>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            ))
-                                        }
+                                                    </div>
+                                                </td>
+                                                <td className="px-12 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                                                    <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
+                                                        <span className="h-1.5 w-1.5 rounded-full bg-emerald-500"></span>
+                                                        <h2 className="text-sm font-normal text-emerald-500">{user.address.city}</h2>
+                                                    </div>
+                                                </td>
+                                                <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{user.email}</td>
+                                                <td className="px-4 py-4 text-sm whitespace-nowrap">
+                                                    <div className="flex items-center gap-x-6">
+                                                        <button onClick={() => deleteUser(user.id)} className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                            </svg>
+                                                        </button>
+                                                        <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                            </svg>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
@@ -133,6 +115,7 @@ const User = () => {
                 </div>
             </section>
         </div>
-    )
-}
-export default User
+    );
+};
+
+export default User;
